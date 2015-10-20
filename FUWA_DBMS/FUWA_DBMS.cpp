d8099d7f2a8cb1126cb0484fc4e84f4dc963dd0a
Feiran Huang <hfr1992@163.com>
2015-10-20 17:25:06 +0800

    Beginning of the project.

---
+++ FUWA_DBMS/FUWA_DBMS.cpp
@@ -0,0 +1,15 @@
+// FUWA_DBMS.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+	printf("Hello, FUWAs!\n");
+	system("pause");
+    return 0;
+}
+
